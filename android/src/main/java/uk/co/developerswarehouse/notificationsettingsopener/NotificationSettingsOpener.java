2265844545ec11565b222d8d1b263cbbaef45292
Simon Hughes <simon.hughes@developerswarehouse.co.uk>
2024-04-05 22:57:45 +0100

    Fix missing typehint

--- android/src/main/java/uk/co/developerswarehouse/notificationsettingsopener/NotificationSettingsOpener.java
+++ android/src/main/java/uk/co/developerswarehouse/notificationsettingsopener/NotificationSettingsOpener.java
@@ -1,6 +1,7 @@
 package uk.co.developerswarehouse.notificationsettingsopener;
 
 import android.util.Log;
+import com.getcapacitor.PluginCall;
 
 public class NotificationSettingsOpener {
     public void openNotificationSettings(PluginCall call) {
